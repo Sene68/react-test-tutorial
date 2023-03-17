c57e1739f3db354a7db6394d2305cac69a5dc21e
Sene68 <senefull68@gmail.com>
2023-03-17 09:51:01 +0900

    coverageThreshold

--- src/components/greet/greet.test.tsx
+++ src/components/greet/greet.test.tsx
@@ -7,10 +7,4 @@ describe('Greet', () => {
         const textElement = screen.getByText(/hello/i)
         expect(textElement).toBeInTheDocument()
     })
-    
-    test('renders with a name', () => {
-        render(<Greet name='Vishwas' />)
-        const textElement = screen.getByText('Hello Vishwas')
-        expect(textElement).toBeInTheDocument()
-    })
 })
