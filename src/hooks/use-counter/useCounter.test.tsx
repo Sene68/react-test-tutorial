7f9470d2279203ce426e2660f7681da02ae675c0
Sene68 <senefull68@gmail.com>
2023-04-08 00:03:44 +0900

    act decrement

--- src/hooks/use-counter/useCounter.test.tsx
+++ src/hooks/use-counter/useCounter.test.tsx
@@ -21,4 +21,14 @@ describe('useCounter', () => {
         act(() => result.current.increment())
         expect(result.current.count).toBe(1)
     })
+
+    test('should decrement the count' , () => {
+        const { result } = renderHook(useCounter, {
+            initialProps: {
+                initialCount: 10
+            }
+        })
+        act(() => result.current.decrement())
+        expect(result.current.count).toBe(9)
+    })
 })
